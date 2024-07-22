4d7e16d0be0591044fbe1e7e7ffd87b2df3f412e
leejinwoo <leejinwoo1126@gmail.com>
2024-07-22 13:45:13 +0900

    git init

---
+++ src/main/java/hello/caching/HelloCachingApplication.java
@@ -0,0 +1,13 @@
+package hello.caching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloCachingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/caching/HelloCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.caching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
